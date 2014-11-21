2dc82525fb08840099e76060c4093f4a85b5dd85
Kevin Lee <kevinmlee.x@gmail.com>
2014-11-20 22:11:35 -0800

    add newlines to screenshot mkdwn

--- screenshots/genMD.py
+++ screenshots/genMD.py
@@ -3,6 +3,6 @@ import os
 with open('README.md', 'w') as mkdn:
 	mkdn.write("Screenshots\n===\n")
 	for f in [f for f in os.listdir('.') if os.path.isfile(f)]:
-		mkdn.write("`%s`" % f)
-		mkdn.write("![image](%s)\n" % f)
+		mkdn.write("`%s`\n\n" % f)
+		mkdn.write("![image](%s)\n\n" % f)
 
