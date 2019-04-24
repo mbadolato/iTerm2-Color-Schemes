ee23a6bc5b6e026e9beca934aa217765294024f5
Daniel Memije <daniel.memije@conekta.com>
2019-04-24 00:36:12 -0500

    Add Andromeda theme (#183)

---
+++ pantheonterminal/Andromeda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Andromeda'
+cursor-color='#f8f8f0'
+foreground='#e5e5e5'
+background='rgba(38,42,51,.95)'
+palette='#000000:#cd3131:#05bc79:#e5e512:#2472c8:#bc3fbc:#0fa8cd:#e5e5e5:#666666:#cd3131:#05bc79:#e5e512:#2472c8:#bc3fbc:#0fa8cd:#e5e5e5'
+COLORS
