20d766b6e175e561629110bcdf45d703b2756349
gdsrosa <gabrieldsrosa@gmail.com>
2019-05-17 18:26:55 -0300

    (+) Add Subliminal theme
    This commit will add a brand new theme for iTerm2

---
+++ pantheonterminal/Subliminal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Subliminal'
+cursor-color='#c7c7c7'
+foreground='#d4d4d4'
+background='rgba(40,44,53,.95)'
+palette='#7f7f7f:#e15a60:#a9cfa4:#ffe2a9:#6699cc:#f1a5ab:#5fb3b3:#d4d4d4:#7f7f7f:#e15a60:#a9cfa4:#ffe2a9:#6699cc:#f1a5ab:#5fb3b3:#d4d4d4'
+COLORS
