3b9474ab283abc71ed85715c602a7debcf85d481
Paul Krueger <mrkrueger@gmail.com>
2019-06-15 20:56:40 -0700

    Add Midnight In Mojave theme

---
+++ dynamic-colors/midnight-in-mojave.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# midnight-in-mojave
+printf "\033]4;0;#1e1e1e;1;#ff453a;2;#32d74b;3;#ffd60a;4;#0a84ff;5;#bf5af2;6;#5ac8fa;7;#ffffff;8;#1e1e1e;9;#ff453a;10;#32d74b;11;#ffd60a;12;#0a84ff;13;#bf5af2;14;#5ac8fa;15;#ffffff\007"
+printf "\033]10;#ffffff;#1e1e1e;#32d74b\007"
+printf "\033]17;#4a504d\007"
+printf "\033]19;#ffffff\007"
+printf "\033]5;0;#ffffff\007"
---
+++ pantheonterminal/midnight-in-mojave.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='midnight-in-mojave'
+cursor-color='#32d74b'
+foreground='#ffffff'
+background='rgba(30,30,30,.95)'
+palette='#1e1e1e:#ff453a:#32d74b:#ffd60a:#0a84ff:#bf5af2:#5ac8fa:#ffffff:#1e1e1e:#ff453a:#32d74b:#ffd60a:#0a84ff:#bf5af2:#5ac8fa:#ffffff'
+COLORS
