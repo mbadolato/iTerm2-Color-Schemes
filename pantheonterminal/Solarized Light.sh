881ad92af4eead52ceb69409b293ad111f1bcecd
Antoine Cotten <hello@acotten.com>
2019-06-29 20:56:38 +0200

    Update built-in Solarized schemes (#194)
    * Update built-in Solarized schemes
    
    Exported from iTerm2 build 3.2.9. Solarized colors are accurate in this
    build, so we don't need duplicates anymore.
    
    Update screenshots for built-in Solarized schemes
    
    * Regenerate all Solarized schemes

--- pantheonterminal/Solarized Light.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dconf load /org/pantheon/terminal/settings/ <<COLORS
-[/]
-name='Solarized Light'
-cursor-color='#536870'
-foreground='#536870'
-background='rgba(252,244,220,.95)'
-palette='#002831:#d11c24:#738a05:#a57706:#2176c7:#c61c6f:#259286:#eae3cb:#001e27:#bd3613:#475b62:#536870:#708284:#5956ba:#819090:#fcf4dc'
-COLORS
