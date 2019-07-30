87af1a201fbf025de5375802a464712036826da5
JustinGrote <justingrote+github@gmail.com>
2019-07-30 08:09:46 -0700

    Feature: Add vscode themes

---
+++ tools/windowsterminal2vscode.ps1
@@ -0,0 +1,32 @@
+#requires -version 5
+if (-not (Test-Path ../vscode)) {New-Item -ItemType Directory ../vscode}
+Get-Item ../WindowsTerminal/*.json | Foreach-Object {
+    write-verbose "Generating vscode theme for $($PSItem.name)"
+    $theme = Get-Content -raw $PSItem | ConvertFrom-JSON
+    @"
+{
+    "workbench.colorCustomizations": {
+        "terminal.foreground": "$($theme.foreground)",
+        "terminal.background": "$($theme.background)",
+        "terminal.ansiBlack": "$($theme.black)",
+        "terminal.ansiBlue": "$($theme.blue)",
+        "terminal.ansiCyan": "$($theme.cyan)",
+        "terminal.ansiGreen": "$($theme.green)",
+        "terminal.ansiMagenta": "$($theme.purple)",
+        "terminal.ansiRed": "$($theme.red)",
+        "terminal.ansiWhite": "$($theme.white)",
+        "terminal.ansiYellow": "$($theme.yellow)",
+        "terminal.ansiBrightBlack": "$($theme.brightBlack)",
+        "terminal.ansiBrightBlue": "$($theme.brightBlue)",
+        "terminal.ansiBrightCyan": "$($theme.brightCyan)",
+        "terminal.ansiBrightGreen": "$($theme.brightGreen)",
+        "terminal.ansiBrightMagenta": "$($theme.brightPurple)",
+        "terminal.ansiBrightRed": "$($theme.brightRed)",
+        "terminal.ansiBrightWhite": "$($theme.brightWhite)",
+        "terminal.ansiBrightYellow": "$($theme.brightYellow)"
+    }
+}
+"@ > "../vscode/$($PSItem.Name)"
+}
+
+
