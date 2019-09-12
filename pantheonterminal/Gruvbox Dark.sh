2a83caad563a7c5e3eb62c89382eccd0987b0f28
Mark Badolato <mbadolato@gmail.com>
2019-09-12 16:22:07 -0700

    Add missing formats and fix a couple of screenshots

--- pantheonterminal/Gruvbox Dark.sh
+++ pantheonterminal/Gruvbox Dark.sh
@@ -2,8 +2,8 @@
 dconf load /org/pantheon/terminal/settings/ <<COLORS
 [/]
 name='Gruvbox Dark'
-cursor-color='#bbbbbb'
+cursor-color='#e6d4a3'
 foreground='#e6d4a3'
 background='rgba(30,30,30,.95)'
-palette='#161819:#f73028:#aab01e:#f7b125:#719586:#c77089:#7db669:#faefbb:#7f7061:#be0f17:#868715:#cc881a:#377375:#a04b73:#578e57:#e6d4a3'
+palette='#1e1e1e:#be0f17:#868715:#cc881a:#377375:#a04b73:#578e57:#978771:#7f7061:#f73028:#aab01e:#f7b125:#719586:#c77089:#7db669:#e6d4a3'
 COLORS
