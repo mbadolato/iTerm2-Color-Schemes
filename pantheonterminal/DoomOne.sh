98b1b77d755958a9aeb5cce364b33d45f4dc78a7
Dmitry Getmantsov <mgck@tuta.io>
2020-01-28 07:25:22 +0200

    Add Doom One theme (#221)
    * Add Doom One theme
    
    * Generate all formats for Doom One theme

---
+++ pantheonterminal/DoomOne.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='DoomOne'
+cursor-color='#51afef'
+foreground='#bbc2cf'
+background='rgba(40,44,52,.95)'
+palette='#000000:#ff6c6b:#98be65:#ecbe7b:#a9a1e1:#c678dd:#51afef:#bbc2cf:#000000:#ff6655:#99bb66:#ecbe7b:#a9a1e1:#c678dd:#51afef:#bfbfbf'
+COLORS
