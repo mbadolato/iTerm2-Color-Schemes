0eba25b4fed4a23a87cfe50dbe7d4f972e2e698a
fitz123 <anton.lugovoi@yandex.ru>
2020-02-10 03:45:50 +0000

    Add BlueBerryPie theme

---
+++ pantheonterminal/BlueBerryPie.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='BlueBerryPie'
+cursor-color='#fcfad6'
+foreground='#babab9'
+background='rgba(28,12,40,.95)'
+palette='#0a4c62:#99246e:#5cb1b3:#eab9a8:#90a5bd:#9d54a7:#7e83cc:#f0e8d6:#201637:#c87272:#0a6c7e:#7a3188:#39173d:#bc94b7:#5e6071:#0a6c7e'
+COLORS
