e401c32853c7726b355b189b1a6de67885587830
Ben Smith <37027883+Smithbm2316@users.noreply.github.com>
2020-03-02 13:08:00 -0800

    adding synthwave-everything theme (#224)
    * adding synthwave-everything theme
    
    * updated README.md
    
    * added README credit, tweaked colors of iTerm theme

---
+++ dynamic-colors/synthwave-everything.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# synthwave-everything
+printf "\033]4;0;#fefefe;1;#f97e72;2;#72f1b8;3;#fede5d;4;#6d77b3;5;#c792ea;6;#f772e0;7;#fefefe;8;#fefefe;9;#f88414;10;#72f1b8;11;#fff951;12;#36f9f6;13;#e1acff;14;#f92aad;15;#fefefe\007"
+printf "\033]10;#f0eff1;#2a2139;#72f1b8\007"
+printf "\033]17;#181521\007"
+printf "\033]19;#f0eff1\007"
+printf "\033]5;0;#f0eff1\007"
---
+++ pantheonterminal/synthwave-everything.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='synthwave-everything'
+cursor-color='#72f1b8'
+foreground='#f0eff1'
+background='rgba(42,33,57,.95)'
+palette='#fefefe:#f97e72:#72f1b8:#fede5d:#6d77b3:#c792ea:#f772e0:#fefefe:#fefefe:#f88414:#72f1b8:#fff951:#36f9f6:#e1acff:#f92aad:#fefefe'
+COLORS
