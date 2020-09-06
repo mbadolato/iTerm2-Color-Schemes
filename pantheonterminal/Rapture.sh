179460039f89bc44f333d6ce3fef714a1b4d2706
Loris Bettazza <loris.bettazza@gmail.com>
2020-09-06 15:14:16 +0200

    Generate other files with update_all script

---
+++ pantheonterminal/Rapture.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Rapture'
+cursor-color='#ffffff'
+foreground='#c0c9e5'
+background='rgba(17,30,42,.95)'
+palette='#000000:#fc644d:#7afde1:#fff09b:#6c9bf5:#ff4fa1:#64e0ff:#c0c9e5:#304b66:#fc644d:#7afde1:#fff09b:#6c9bf5:#ff4fa1:#64e0ff:#ffffff'
+COLORS
