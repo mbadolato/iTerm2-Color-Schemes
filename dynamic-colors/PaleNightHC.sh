49678535288c6a8b57ef8ec56a62bd2c50ec998d
Kasper <kasperliu129@gmail.com>
2021-02-04 21:08:51 +0800

    Added PaleNight Theme

---
+++ dynamic-colors/PaleNightHC.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# PaleNightHC
+printf "\033]4;0;#000000;1;#f07178;2;#c3e88d;3;#ffcb6b;4;#82aaff;5;#c792ea;6;#89ddff;7;#ffffff;8;#323232;9;#f38d93;10;#cfeca3;11;#ffd588;12;#9bbbff;13;#d2a7ee;14;#a0e3ff;15;#cccccc\007"
+printf "\033]10;#cccccc;#3e4251;#ffcb6b\007"
+printf "\033]17;#717cb4\007"
+printf "\033]19;#80cbc4\007"
+printf "\033]5;0;#ffffff\007"
---
+++ pantheonterminal/PaleNightHC.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='PaleNightHC'
+cursor-color='#ffcb6b'
+foreground='#cccccc'
+background='rgba(62,66,81,.95)'
+palette='#000000:#f07178:#c3e88d:#ffcb6b:#82aaff:#c792ea:#89ddff:#ffffff:#323232:#f38d93:#cfeca3:#ffd588:#9bbbff:#d2a7ee:#a0e3ff:#cccccc'
+COLORS
