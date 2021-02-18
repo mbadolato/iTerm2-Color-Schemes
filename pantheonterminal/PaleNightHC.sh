6e931041d9e4239c1a49d79499ed3292f6734c24
Raziel Anarki <gergely.pap@centralmediacsoport.hu>
2021-02-18 01:08:52 +0100

    PaleNightHC ported definitions, re-generated with update_all.py to match the definition in schemes/PaleNightHC.itermcolors

--- pantheonterminal/PaleNightHC.sh
+++ pantheonterminal/PaleNightHC.sh
@@ -5,5 +5,5 @@ name='PaleNightHC'
 cursor-color='#ffcb6b'
 foreground='#cccccc'
 background='rgba(62,66,81,.95)'
-palette='#000000:#f07178:#c3e88d:#ffcb6b:#82aaff:#c792ea:#89ddff:#ffffff:#323232:#f38d93:#cfeca3:#ffd588:#9bbbff:#d2a7ee:#a0e3ff:#cccccc'
+palette='#000000:#f07178:#c3e88d:#ffcb6b:#82aaff:#c792ea:#89ddff:#ffffff:#666666:#f6a9ae:#dbf1ba:#ffdfa6:#b4ccff:#ddbdf2:#b8eaff:#999999'
 COLORS
