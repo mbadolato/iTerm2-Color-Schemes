d70787b0ca5b4c937ea17ea291c1debb51443f89
Pratik <pratik@AlphaTPadMac.local>
2021-05-24 14:07:03 +0530

    Added new colorscheme breeze light

---
+++ pantheonterminal/Breeze_Light.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Breeze_Light'
+cursor-color='#000000'
+foreground='#383a42'
+background='rgba(250,250,250,.95)'
+palette='#383a42:#e4564a:#50a050:#c29200:#0070d4:#a626a4:#0a96b4:#fafafa:#1c1a24:#f83636:#50c850:#dec505:#005ce8:#ba0eb8:#00aac8:#fefefe'
+COLORS
