ce3ea8037b990923096dc4e1d949d55c26db4413
Pratik <pratik@Pratiks-MBP.local>
2021-06-12 00:09:46 +0530

    Autogenerated colorschemes using update_all script.

---
+++ pantheonterminal/HaX0R_BLUE.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='HaX0R_BLUE'
+cursor-color='#10b6ff'
+foreground='#11b7ff'
+background='rgba(1,5,21,.95)'
+palette='#010921:#10b6ff:#10b6ff:#10b6ff:#10b6ff:#10b6ff:#10b6ff:#fafafa:#080117:#00b3f7:#00b3f7:#00b3f7:#00b3f7:#00b3f7:#00b3f7:#fefefe'
+COLORS
---
+++ pantheonterminal/HaX0R_GR33N.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='HaX0R_GR33N'
+cursor-color='#15d00d'
+foreground='#16b10e'
+background='rgba(2,15,1,.95)'
+palette='#001f0b:#15d00d:#15d00d:#15d00d:#15d00d:#15d00d:#15d00d:#fafafa:#001510:#19e20e:#19e20e:#19e20e:#19e20e:#19e20e:#19e20e:#fefefe'
+COLORS
