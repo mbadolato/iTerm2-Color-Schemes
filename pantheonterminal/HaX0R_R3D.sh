c12b3f54fde26684543161522e83455011cc7fc7
Pratik Mullick <41203200+pratikmullick@users.noreply.github.com>
2021-06-13 21:28:40 +0530

    Added 3 New Monochrome Themes called HaX0R (#304)
    * Added HaX0R red, green and blue colorschemes
    
    These colorschemes are for iTerm2.
    
    * Autogenerated colorschemes using update_all script.
    
    * Added screenshots
    
    * Modified README
    
    Co-authored-by: Pratik <pratik@Pratiks-MBP.local>
    Co-authored-by: Mark Badolato <mbadolato@gmail.com>

---
+++ pantheonterminal/HaX0R_R3D.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='HaX0R_R3D'
+cursor-color='#b00d0d'
+foreground='#b10e0e'
+background='rgba(32,1,1,.95)'
+palette='#1f0000:#b00d0d:#b00d0d:#b00d0d:#b00d0d:#b00d0d:#b00d0d:#fafafa:#150000:#ff1111:#ff1010:#ff1010:#ff1010:#ff1010:#ff1010:#fefefe'
+COLORS
