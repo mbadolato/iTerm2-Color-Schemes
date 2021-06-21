9f2d8f9113a6a67a6e894f3c5f0ad03376378616
Al Duncanson <alexjduncanson@gmail.com>
2021-06-21 19:14:56 -0400

    add GitHub Dark theme

---
+++ pantheonterminal/GitHub Dark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='GitHub Dark'
+cursor-color='#c9d1d9'
+foreground='#8b949e'
+background='rgba(16,18,22,.95)'
+palette='#000000:#f78166:#56d364:#e3b341:#6ca4f8:#db61a2:#2b7489:#ffffff:#4d4d4d:#f78166:#56d364:#e3b341:#6ca4f8:#db61a2:#2b7489:#ffffff'
+COLORS
