800a6bb6d6b1fc8d1d403041741a56cd01b90818
barish <31482356+heyitsbarish@users.noreply.github.com>
2021-07-22 16:13:44 -0500

    Adding Abernathy theme to project

---
+++ pantheonterminal/Abernathy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Abernathy'
+cursor-color='#bbbbbb'
+foreground='#eeeeec'
+background='rgba(17,20,22,.95)'
+palette='#000000:#cd0000:#00cd00:#cdcd00:#1093f5:#cd00cd:#00cdcd:#faebd7:#404040:#ff0000:#00ff00:#ffff00:#11b5f6:#ff00ff:#00ffff:#ffffff'
+COLORS
