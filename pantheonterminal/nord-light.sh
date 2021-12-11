f2d6355fad21c7961a3d8911759244bafcaf4589
Bror Bisgaard Winther <bbw@clearhaus.com>
2021-12-11 06:26:13 +0100

    generate nord-light

---
+++ pantheonterminal/nord-light.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='nord-light'
+cursor-color='#88c0d0'
+foreground='#414858'
+background='rgba(229,233,240,.95)'
+palette='#3b4252:#bf616a:#a3be8c:#ebcb8b:#81a1c1:#b48ead:#88c0d0:#d8dee9:#4c566a:#bf616a:#a3be8c:#ebcb8b:#81a1c1:#b48ead:#8fbcbb:#eceff4'
+COLORS
