aeace8f977b7baab2c943babf7fdea9521e261b3
Jackson Robbins <jackrobbins1@gmail.com>
2022-02-07 15:38:19 -0600

    Fixed schemes/Blazer.itermcolors to include missing properties: bold color, selection color, selected text color. Ran 'python3 update_all.py' to generate all themes for updated Blazer scheme.

--- dynamic-colors/Blazer.sh
+++ dynamic-colors/Blazer.sh
@@ -2,3 +2,6 @@
 # Blazer
 printf "\033]4;0;#000000;1;#b87a7a;2;#7ab87a;3;#b8b87a;4;#7a7ab8;5;#b87ab8;6;#7ab8b8;7;#d9d9d9;8;#262626;9;#dbbdbd;10;#bddbbd;11;#dbdbbd;12;#bdbddb;13;#dbbddb;14;#bddbdb;15;#ffffff\007"
 printf "\033]10;#d9e6f2;#0d1926;#d9e6f2\007"
+printf "\033]17;#c1ddff\007"
+printf "\033]19;#000000\007"
+printf "\033]5;0;#ffffff\007"
