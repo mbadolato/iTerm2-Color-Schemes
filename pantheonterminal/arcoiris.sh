c30b761ff988f46bdb3a1c342773dcbc25ba4d27
indyandie <1058837+Indyandie@users.noreply.github.com>
2022-06-12 00:17:55 -0500

    generate arcoiris formats

---
+++ pantheonterminal/arcoiris.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='arcoiris'
+cursor-color='#7a1c1c'
+foreground='#eee4d9'
+background='rgba(32,31,30,.95)'
+palette='#333333:#da2700:#12c258:#ffc656:#518bfc:#e37bd9:#63fad5:#bab2b2:#777777:#ffb9b9:#e3f6aa:#ffddaa:#b3e8f3:#cbbaf9:#bcffc7:#efefef'
+COLORS
