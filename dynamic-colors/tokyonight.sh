e12749735bd6ccd0ac3e9e77e03bd2f58ea3c655
thomas <thomas.croft@pm.me>
2022-06-14 16:46:03 -0600

    TokyoNight

---
+++ dynamic-colors/tokyonight.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# tokyonight
+printf "\033]4;0;#15161e;1;#f7768e;2;#9ece6a;3;#e0af68;4;#7aa2f7;5;#bb9af7;6;#7dcfff;7;#a9b1d6;8;#414868;9;#f7768e;10;#9ece6a;11;#e0af68;12;#7aa2f7;13;#bb9af7;14;#7dcfff;15;#c0caf5\007"
+printf "\033]10;#c0caf5;#1a1b26;#c0caf5\007"
+printf "\033]17;#33467c\007"
+printf "\033]19;#c0caf5\007"
+printf "\033]5;0;#eeeeee\007"
---
+++ pantheonterminal/tokyonight-storm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='tokyonight-storm'
+cursor-color='#c0caf5'
+foreground='#c0caf5'
+background='rgba(36,40,59,.95)'
+palette='#1d202f:#f7768e:#9ece6a:#e0af68:#7aa2f7:#bb9af7:#7dcfff:#a9b1d6:#414868:#f7768e:#9ece6a:#e0af68:#7aa2f7:#bb9af7:#7dcfff:#c0caf5'
+COLORS
