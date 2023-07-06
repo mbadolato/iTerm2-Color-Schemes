9ef075e7100d27a4b3131b0db81ef6d48063882c
Daniel Weibel <danielmweibel@gmail.com>
2023-07-06 17:43:57 +0200

    Generate other formats of "iTerm2 Default" scheme

---
+++ dynamic-colors/iTerm2 Default.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# iTerm2 Default
+printf "\033]4;0;#000000;1;#c91b00;2;#00c200;3;#c7c400;4;#2225c4;5;#ca30c7;6;#00c5c7;7;#ffffff;8;#686868;9;#ff6e67;10;#5ffa68;11;#fffc67;12;#6871ff;13;#ff77ff;14;#60fdff;15;#ffffff\007"
+printf "\033]10;#ffffff;#000000;#e5e5e5\007"
+printf "\033]17;#c1deff\007"
+printf "\033]19;#000000\007"
+printf "\033]5;0;#ffffff\007"
