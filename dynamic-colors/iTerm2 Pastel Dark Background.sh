7bcb511fad6502c7ccb044ba7137e017a1aa77cc
Daniel Weibel <danielmweibel@gmail.com>
2023-07-28 22:04:30 +0200

    Update iTerm2 built-in colour schemes (#411)
    * Delete all old iTerm2 builtin schemes
    
    Including screenshots, references in READMEs, etc.
    
    * Add .itermcolors file of all built-in iTerm2 colour schemes
    
    iTerm2 Build 3.4.19
    
    * Generate all formats
    
    * Add screenshots
    
    * Add screenshots to README
    
    * Add note about iTerm2 schemes in Credits section of README
    
    * Remove entry for 'iTerm2 Default' from CREDITS.md
    
    * Add links to screenshots to listed schemes in Credits section of README
    
    * Fix script name in "How to add a new theme" instructions

---
+++ dynamic-colors/iTerm2 Pastel Dark Background.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# iTerm2 Pastel Dark Background
+printf "\033]4;0;#626262;1;#ff8373;2;#b4fb73;3;#fffdc3;4;#a5d5fe;5;#ff90fe;6;#d1d1fe;7;#f1f1f1;8;#8f8f8f;9;#ffc4be;10;#d6fcba;11;#fffed5;12;#c2e3ff;13;#ffb2fe;14;#e6e6fe;15;#ffffff\007"
+printf "\033]10;#c7c7c7;#000000;#ffb473\007"
+printf "\033]17;#454d96\007"
+printf "\033]19;#f4f4f4\007"
+printf "\033]5;0;#ffffff\007"
---
+++ pantheonterminal/iTerm2 Smoooooth.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='iTerm2 Smoooooth'
+cursor-color='#ffffff'
+foreground='#dcdcdc'
+background='rgba(21,25,31,.95)'
+palette='#14191e:#b43c2a:#00c200:#c7c400:#2744c7:#c040be:#00c5c7:#c7c7c7:#686868:#dd7975:#58e790:#ece100:#a7abf2:#e17ee1:#60fdff:#ffffff'
+COLORS
