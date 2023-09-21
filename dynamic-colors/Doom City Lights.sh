a7a4d5068007724ee28a39597c7ed18b7740c3ae
JoshBlu <>
2023-09-20 22:22:58 -0500

    Removed 'Doom City Lights' themes as required documentation has not been updated

--- dynamic-colors/Doom City Lights.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Doom City Lights
-printf "\033]4;0;#10151c;1;#d95468;2;#8bd49c;3;#ebbf83;4;#539afc;5;#b62d65;6;#008b94;7;#a0b3c5;8;#41505e;9;#d98e48;10;#33ced8;11;#f7dab3;12;#5ec4ff;13;#e27e8d;14;#70e1e8;15;#728ca0\007"
-printf "\033]10;#a0b3c5;#1d252c;#5ec4ff\007"
-printf "\033]17;#718ca1\007"
-printf "\033]19;#b3ecff\007"
-printf "\033]5;0;#f8f8f8\007"
--- pantheonterminal/Doom City Lights.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dconf load /org/pantheon/terminal/settings/ <<COLORS
-[/]
-name='Doom City Lights'
-cursor-color='#5ec4ff'
-foreground='#a0b3c5'
-background='rgba(29,37,44,.95)'
-palette='#10151c:#d95468:#8bd49c:#ebbf83:#539afc:#b62d65:#008b94:#a0b3c5:#41505e:#d98e48:#33ced8:#f7dab3:#5ec4ff:#e27e8d:#70e1e8:#728ca0'
-COLORS
