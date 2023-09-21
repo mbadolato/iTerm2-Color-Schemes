c52c9d5ca9a871f55708c8caef00f6487b03b907
JoshBlu <>
2023-09-20 22:24:59 -0500

    Revert "Removed 'Doom City Lights' themes as required documentation has not been updated"
    This reverts commit a7a4d5068007724ee28a39597c7ed18b7740c3ae.

---
+++ pantheonterminal/Bright Lights.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Bright Lights'
+cursor-color='#f34b00'
+foreground='#b3c9d7'
+background='rgba(25,25,25,.95)'
+palette='#191919:#ff355b:#b7e876:#ffc251:#76d4ff:#ba76e7:#6cbfb5:#c2c8d7:#191919:#ff355b:#b7e876:#ffc251:#76d5ff:#ba76e7:#6cbfb5:#c2c8d7'
+COLORS
