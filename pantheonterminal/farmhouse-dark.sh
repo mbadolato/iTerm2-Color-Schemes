b0b858d1d9363ab22052716b6bab5c62be55b529
clach04 <Chris.Clark@actian.com>
2024-01-22 12:14:29 -0800

    Import/Generate Farmhouse (#428)
    From https://github.com/mattly/iterm-colors-farmhouse
    by Matthew Lyon

---
+++ pantheonterminal/farmhouse-dark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='farmhouse-dark'
+cursor-color='#006efe'
+foreground='#e8e4e1'
+background='rgba(29,32,39,.95)'
+palette='#1d2027:#ba0004:#549d00:#c87300:#0049e6:#9f1b61:#1fb65c:#e8e4e1:#394047:#eb0009:#7ac100:#ea9a00:#006efe:#bf3b7f:#19e062:#f4eef0'
+COLORS
