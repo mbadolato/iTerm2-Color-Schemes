0aea65703fa8ae7d3338c871dca2cafc7c265343
Tobias Fried <friedtm@gmail.com>
2024-04-17 02:27:35 -0600

    Add Mellifluous theme

---
+++ pantheonterminal/Mellifluous.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Mellifluous'
+cursor-color='#bfad9e'
+foreground='#dadada'
+background='rgba(26,26,26,.95)'
+palette='#1a1a1a:#d29393:#b3b393:#cbaa89:#a8a1be:#b39fb0:#c0af8c:#dadada:#5b5b5b:#c95954:#828040:#a6794c:#5a6599:#9c6995:#74a39e:#ffffff'
+COLORS
