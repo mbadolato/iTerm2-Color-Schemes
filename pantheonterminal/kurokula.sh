e9b879653d099484ce2fe0938e0ae53eef1a92be
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-05-31 22:30:29 -0500

    feat: kurokula theme formats

---
+++ pantheonterminal/kurokula.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='kurokula'
+cursor-color='#702420'
+foreground='#ddd0c4'
+background='rgba(20,21,21,.95)'
+palette='#333333:#d23e08:#54ca74:#fff700:#2ab9ff:#9e60ec:#9d5918:#ddd0c4:#141515:#b66056:#85b1a9:#dbbb43:#6890d7:#887aa3:#837369:#94959b'
+COLORS
