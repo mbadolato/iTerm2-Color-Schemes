0b9c9f53e2de134ee448f01d3d1acff708a85933
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-05-31 23:39:40 -0500

    feat: kurokula refactor colors

--- pantheonterminal/kurokula.sh
+++ pantheonterminal/kurokula.sh
@@ -5,5 +5,5 @@ name='kurokula'
 cursor-color='#702420'
 foreground='#ddd0c4'
 background='rgba(20,21,21,.95)'
-palette='#333333:#d23e08:#54ca74:#fff700:#2ab9ff:#9e60ec:#9d5918:#ddd0c4:#141515:#b66056:#85b1a9:#dbbb43:#6890d7:#887aa3:#837369:#94959b'
+palette='#333333:#b66056:#85b1a9:#dbbb43:#6890d7:#887aa3:#837369:#ddd0c4:#515151:#ffc663:#c1ffae:#fff700:#a1d9ff:#a994ff:#f9cfb9:#ffffff'
 COLORS
