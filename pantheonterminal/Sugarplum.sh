e7ce0103167b18f66df2c18b0f51f0218f7c87d0
lemonlime0x3C33 <169304592+lemonlime0x3C33@users.noreply.github.com>
2024-06-02 22:11:38 +0200

    fixed bright colors

--- pantheonterminal/Sugarplum.sh
+++ pantheonterminal/Sugarplum.sh
@@ -5,5 +5,5 @@ name='Sugarplum'
 cursor-color='#53b397'
 foreground='#db7ddd'
 background='rgba(17,17,71,.95)'
-palette='#111147:#5ca8dc:#53b397:#249a84:#db7ddd:#d0beee:#b5e4f4:#a175d4:#111147:#5ca8dc:#53b397:#249a84:#db7ddd:#d0beee:#cdab53:#a175d4'
+palette='#111147:#5ca8dc:#53b397:#249a84:#db7ddd:#d0beee:#f9f3f9:#a175d4:#111147:#5cb5dc:#52deb5:#01f5c7:#fa5dfd:#c6a5fd:#ffffff:#b577fd'
 COLORS
