2bf2a1cbfa2906f85540365e19172163f1292380
lemonlime0x3C33 <169304592+lemonlime0x3C33@users.noreply.github.com>
2024-06-02 22:37:55 +0200

    playing with bold color

--- dynamic-colors/Sugarplum.sh
+++ dynamic-colors/Sugarplum.sh
@@ -4,4 +4,4 @@ printf "\033]4;0;#111147;1;#5ca8dc;2;#53b397;3;#249a84;4;#db7ddd;5;#d0beee;6;#f9
 printf "\033]10;#db7ddd;#111147;#53b397\007"
 printf "\033]17;#5ca8dc\007"
 printf "\033]19;#d0beee\007"
-printf "\033]5;0;#a175d4\007"
+printf "\033]5;0;#d0beee\007"
