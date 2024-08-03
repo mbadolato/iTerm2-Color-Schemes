bffab29f416d2603cd032c1fd3a800814be8eadb
hanna <hanna@hanna.lol>
2024-08-03 14:46:43 -0700

    Add oxocarbon theme by nyoom engineering

---
+++ pantheonterminal/Oxocarbon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Oxocarbon'
+cursor-color='#000000'
+foreground='#393939'
+background='rgba(242,244,248,.95)'
+palette='#f2f4f8:#ff7eb6:#0f62fe:#ff6f00:#42be65:#be95ff:#673ab7:#393939:#161616:#ff7eb6:#0f62fe:#ff6f00:#42be65:#be95ff:#673ab7:#393939'
+COLORS
