696760d67bfb24fb64c5972aa4378e981e9fc9e1
Alex Nicksay <nicksay@gmail.com>
2024-11-01 14:38:06 -0700

    Add SpaceGray Bright theme (#452)

---
+++ dynamic-colors/SpaceGray Bright.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# SpaceGray Bright
+printf "\033]4;0;#080808;1;#bc5553;2;#a0b56c;3;#f6c987;4;#7baec1;5;#b98aae;6;#85c9b8;7;#d8d8d8;8;#626262;9;#bc5553;10;#a0b56c;11;#f6c987;12;#7baec1;13;#b98aae;14;#85c9b8;15;#f7f7f7\007"
+printf "\033]10;#f3f3f3;#2a2e3a;#c6c6c6\007"
+printf "\033]17;#cacaca\007"
+printf "\033]19;#000000\007"
+printf "\033]5;0;#ffffff\007"
