8033643f77837c7febe00c3cdcc78f33f4b1b963
OlshaMB <michaelolshanskiy@icloud.com>
2024-11-04 00:53:38 +0300

    feat: Add mellow theme

---
+++ pantheonterminal/mellow.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='mellow'
+cursor-color='#cac9dd'
+foreground='#c9c7cd'
+background='rgba(22,22,23,.95)'
+palette='#27272a:#f5a191:#90b99f:#e6b99d:#aca1cf:#e29eca:#ea83a5:#c1c0d4:#353539:#ffae9f:#9dc6ac:#f0c5a9:#b9aeda:#ecaad6:#f591b2:#cac9dd'
+COLORS
