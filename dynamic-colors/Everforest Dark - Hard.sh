80543b14552b7c9fef88fad826552e6ac5632abe
Nicolas GB <ngou0210@gmail.com>
2024-11-12 23:38:18 +0100

    feat: Add Everforest Dark - Hard theme (#455)

---
+++ dynamic-colors/Everforest Dark - Hard.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Everforest Dark - Hard
+printf "\033]4;0;#7a8478;1;#e67e80;2;#a7c080;3;#dbbc7f;4;#7fbbb3;5;#d699b6;6;#83c092;7;#f2efdf;8;#a6b0a0;9;#f85552;10;#8da101;11;#dfa000;12;#3a94c5;13;#df69ba;14;#35a77c;15;#fffbef\007"
+printf "\033]10;#d3c6aa;#1e2326;#e69875\007"
+printf "\033]17;#4c3743\007"
+printf "\033]19;#d3c6aa\007"
+printf "\033]5;0;#dbbc7f\007"
---
+++ pantheonterminal/Everforest Dark - Hard.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Everforest Dark - Hard'
+cursor-color='#e69875'
+foreground='#d3c6aa'
+background='rgba(30,35,38,.95)'
+palette='#7a8478:#e67e80:#a7c080:#dbbc7f:#7fbbb3:#d699b6:#83c092:#f2efdf:#a6b0a0:#f85552:#8da101:#dfa000:#3a94c5:#df69ba:#35a77c:#fffbef'
+COLORS
