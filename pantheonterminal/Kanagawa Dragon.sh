665bb7457c35336d038682e9384ed3b1ade89d58
Jeremy Seago <jeremy.seago@ispot.tv>
2024-11-21 15:06:16 -0600

    feature/add-kanagawa-wave: Follows pull request procedure to add Kanagawa Wave and Kanagawa Dragon from rebelot/kanagawa.nvim

---
+++ pantheonterminal/Kanagawa Dragon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Kanagawa Dragon'
+cursor-color='#c5c9c5'
+foreground='#c8c093'
+background='rgba(24,22,22,.95)'
+palette='#0d0c0c:#c4746e:#8a9a7b:#c4b28a:#8ba4b0:#a292a3:#8ea4a2:#c8c093:#a6a69c:#e46876:#87a987:#e6c384:#7fb4ca:#938aa9:#7aa89f:#c5c9c5'
+COLORS
