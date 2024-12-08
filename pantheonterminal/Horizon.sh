8609400951a10bea05574e45327ab3de16872057
Antonio Camargo <antoniop.camargo@gmail.com>
2024-12-08 14:57:23 -0300

    Add the Horizon theme

---
+++ pantheonterminal/Horizon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Horizon'
+cursor-color='#c7c7c7'
+foreground='#bbbaba'
+background='rgba(28,30,38,.95)'
+palette='#131519:#e95478:#14d386:#fab795:#30aad7:#b877db:#1fdad9:#c7c7c7:#686868:#e06783:#0af29d:#fac39a:#56c2ea:#c38ce1:#3ce8e6:#ffffff'
+COLORS
