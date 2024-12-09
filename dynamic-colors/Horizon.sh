1b407e9b7076989024d7b8594370b082d2e422fb
Antônio Camargo <apcamargo@users.noreply.github.com>
2024-12-09 08:06:30 -0800

    Add the Horizon theme (#462)
    * Add the Horizon theme
    
    * Update

---
+++ dynamic-colors/Horizon.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Horizon
+printf "\033]4;0;#131519;1;#e95478;2;#14d386;3;#fab795;4;#30aad7;5;#b877db;6;#1fdad9;7;#c7c7c7;8;#686868;9;#e06783;10;#0af29d;11;#fac39a;12;#56c2ea;13;#c38ce1;14;#3ce8e6;15;#ffffff\007"
+printf "\033]10;#bbbaba;#1c1e26;#c7c7c7\007"
+printf "\033]17;#292b36\007"
+printf "\033]19;#dfd3d5\007"
+printf "\033]5;0;#ffffff\007"
