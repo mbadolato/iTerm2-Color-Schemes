927edd77c21860b0dce1f8fde68789ab41e7b5de
Bryan Lee <38807139+liby@users.noreply.github.com>
2024-12-27 10:55:15 +0800

    Add Snazzy Soft theme

---
+++ pantheonterminal/Snazzy Soft.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Snazzy Soft'
+cursor-color='#eaeaea'
+foreground='#eff0eb'
+background='rgba(40,42,54,.95)'
+palette='#000000:#ff5c57:#5af78e:#f3f99d:#57c7ff:#ff6ac1:#9aedfe:#f1f1f0:#686868:#ff5c57:#5af78e:#f3f99d:#57c7ff:#ff6ac1:#9aedfe:#f1f1f0'
+COLORS
