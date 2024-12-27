b998fe602981d19a821bb02ce2431cec204d8c29
Filip Janevski <filip@janevski.dev>
2024-12-27 13:03:01 +0100

    Add 0x96f theme

---
+++ dynamic-colors/0x96f.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# 0x96f
+printf "\033]4;0;#262427;1;#ff7272;2;#bcdf59;3;#ffca58;4;#49cae4;5;#a093e2;6;#aee8f4;7;#fcfcfa;8;#545452;9;#ff8787;10;#c6e472;11;#ffd271;12;#64d2e8;13;#aea3e6;14;#baebf6;15;#fcfcfa\007"
+printf "\033]10;#fcfcfa;#262427;#fcfcfa\007"
+printf "\033]17;#fcfcfa\007"
+printf "\033]19;#262427\007"
+printf "\033]5;0;#fcfcfa\007"
