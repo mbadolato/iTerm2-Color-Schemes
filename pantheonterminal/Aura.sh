030073d059c9049707324e91a66436a50fb0e7d2
Sam Martin <me@smartin.io>
2024-12-27 05:22:56 +0100

    Run cd tools && python3 gen.py -s Aura

---
+++ pantheonterminal/Aura.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Aura'
+cursor-color='#a277ff'
+foreground='#edecee'
+background='rgba(21,20,27,.95)'
+palette='#110f18:#ff6767:#61ffca:#ffca85:#a277ff:#a277ff:#61ffca:#edecee:#4d4d4d:#ffca85:#a277ff:#ffca85:#a277ff:#a277ff:#61ffca:#edecee'
+COLORS
