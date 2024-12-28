52275dbd533e5d4dd6a50ff730edb9e215326c46
Mike McBride <mike.mcbride@hey.com>
2024-12-27 23:45:20 -0600

    fix old colors

--- pantheonterminal/electron-highlighter.sh
+++ pantheonterminal/electron-highlighter.sh
@@ -5,5 +5,5 @@ name='electron-highlighter'
 cursor-color='#a8b5d1'
 foreground='#a8b5d1'
 background='rgba(36,40,59,.95)'
-palette='#15161e:#f7768e:#34febb:#ffd9af:#82aaff:#c792ea:#4ff2f8:#7c8eac:#506686:#f7768e:#34febb:#ffd9af:#82aaff:#c792ea:#4ff2f8:#c5cee0'
+palette='#15161e:#f7768e:#58ffc7:#ffd9af:#82aaff:#d2a6ef:#57f9ff:#7c8eac:#506686:#f7768e:#58ffc7:#ffd9af:#82aaff:#d2a6ef:#57f9ff:#c5cee0'
 COLORS
