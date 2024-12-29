7df3371ae4f74f0abf37aa4a3e89dd5ec402b158
Juliano Fernandes <julianofernandes@gmail.com>
2024-12-29 13:50:24 -0300

    Fix Rosé Pine colors

--- pantheonterminal/rose-pine-dawn.sh
+++ pantheonterminal/rose-pine-dawn.sh
@@ -2,8 +2,8 @@
 dconf load /org/pantheon/terminal/settings/ <<COLORS
 [/]
 name='rose-pine-dawn'
-cursor-color='#575279'
+cursor-color='#9893a5'
 foreground='#575279'
 background='rgba(250,244,237,.95)'
-palette='#f2e9e1:#b4637a:#56949f:#ea9d34:#286983:#907aa9:#d7827e:#575279:#9893a5:#b4637a:#56949f:#ea9d34:#286983:#907aa9:#d7827e:#575279'
+palette='#f2e9de:#b4637a:#56949f:#ea9d34:#286983:#907aa9:#d7827e:#575279:#6e6a86:#b4637a:#56949f:#ea9d34:#286983:#907aa9:#d7827e:#575279'
 COLORS
--- pantheonterminal/rose-pine-moon.sh
+++ pantheonterminal/rose-pine-moon.sh
@@ -2,8 +2,8 @@
 dconf load /org/pantheon/terminal/settings/ <<COLORS
 [/]
 name='rose-pine-moon'
-cursor-color='#e0def4'
+cursor-color='#59546d'
 foreground='#e0def4'
 background='rgba(35,33,54,.95)'
-palette='#393552:#eb6f92:#9ccfd8:#f6c177:#3e8fb0:#c4a7e7:#ea9a97:#e0def4:#6e6a86:#eb6f92:#9ccfd8:#f6c177:#3e8fb0:#c4a7e7:#ea9a97:#e0def4'
+palette='#393552:#eb6f92:#9ccfd8:#f6c177:#3e8fb0:#c4a7e7:#ea9a97:#e0def4:#817c9c:#eb6f92:#9ccfd8:#f6c177:#3e8fb0:#c4a7e7:#ea9a97:#e0def4'
 COLORS
