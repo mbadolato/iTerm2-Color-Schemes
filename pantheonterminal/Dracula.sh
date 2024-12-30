9e0ff9ecc800cc1247bc74211f215a158e43036a
Aarni Koskela <akx@iki.fi>
2024-12-30 18:53:23 +0200

    Update Dracula theme from upstream (#490)
    Fixes #487

--- pantheonterminal/Dracula.sh
+++ pantheonterminal/Dracula.sh
@@ -3,7 +3,7 @@ dconf load /org/pantheon/terminal/settings/ <<COLORS
 [/]
 name='Dracula'
 cursor-color='#bbbbbb'
-foreground='#f8f8f2'
+foreground='#e6e6e6'
 background='rgba(30,31,41,.95)'
 palette='#000000:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#bbbbbb:#555555:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#ffffff'
 COLORS
