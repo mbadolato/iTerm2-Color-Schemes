65db078f38503012fd069d425dddd2965b489a47
aron <vndepol@gmail.com>
2024-12-31 15:23:04 +0900

    added Melange light and Dark theme

---
+++ pantheonterminal/Melange_dark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Melange_dark'
+cursor-color='#ece1d7'
+foreground='#ece1d7'
+background='rgba(41,37,34,.95)'
+palette='#34302c:#bd8183:#78997a:#e49b5d:#7f91b2:#b380b0:#7b9695:#c1a78e:#867462:#d47766:#85b695:#ebc06d:#a3a9ce:#cf9bc2:#89b3b6:#ece1d7'
+COLORS
