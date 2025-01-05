aadd3d8483c8af4c78e62aec1a206712756c7c28
Zack McCauley <wpgraphix@gmail.com>
2025-01-05 12:45:55 -0800

    Use tools to create all files for cobaltnext-dark

---
+++ dynamic-colors/CobaltNext-Dark.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# CobaltNext-Dark
+printf "\033]4;0;#282f36;1;#e6576a;2;#99c794;3;#fac863;4;#5a9bcf;5;#c5a5c5;6;#5fb3b3;7;#d8dee9;8;#65737e;9;#d6838c;10;#c1dcbe;11;#ffde9b;12;#8abee7;13;#edcded;14;#9be2e2;15;#ffffff\007"
+printf "\033]10;#d8dee9;#0f1c23;#fac863\007"
+printf "\033]17;#5fb3b3\007"
+printf "\033]19;#ffffff\007"
+printf "\033]5;0;#ffffff\007"
