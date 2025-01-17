0e23daf59234fc892cba949562d7bf69204594bb
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-01-17 11:19:20 -0800

    Fix Horizon Bright (#518)

--- pantheonterminal/Horizon-Bright.sh
+++ pantheonterminal/Horizon-Bright.sh
@@ -2,8 +2,8 @@
 dconf load /org/pantheon/terminal/settings/ <<COLORS
 [/]
 name='Horizon-Bright'
-cursor-color='#f9cec3'
-foreground='#06060c'
-background='rgba(253,240,237,.95)'
-palette='#000000:#e95678:#29d398:#fab795:#26bbd9:#ee64ac:#59e1e3:#555555:#666666:#ec6a88:#3fdaa4:#fbc3a7:#3fc4de:#f075b5:#6be4e6:#a5a5a5'
+cursor-color='#f2d0c5'
+foreground='#16161c'
+background='rgba(251,240,238,.95)'
+palette='#16161c:#e95678:#29d398:#fab795:#26bbd9:#ee64ae:#59e3e3:#fdf0ed:#1a1c23:#ec6a88:#3fdaa4:#fbc3a7:#3fc6de:#f075b7:#6be6e6:#fff3f0'
 COLORS
