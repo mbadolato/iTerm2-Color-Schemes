5c98ca96a752b3b9e51576776b7da06e468d8624
Antonio <vozaanthony@gmail.com>
2025-04-14 21:29:45 +0200

    Create Box theme for Ghostty (#549)
    * Create Box theme for Ghostty
    
    * Update CREDITS.md
    
    * Delete ghostty/Box
    
    * Add Box theme

---
+++ dynamic-colors/Box.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Box
+printf "\033]4;0;#000000;1;#cc0403;2;#19cb00;3;#cecb00;4;#0d73cc;5;#cb1ed1;6;#0dcdcd;7;#dddddd;8;#767676;9;#f2201f;10;#23fd00;11;#fffd00;12;#1a8fff;13;#fd28ff;14;#14ffff;15;#ffffff\007"
+printf "\033]10;#9fef00;#141d2b;#9fef00\007"
+printf "\033]17;#a4b1cd\007"
+printf "\033]19;#141d2b\007"
+printf "\033]5;0;#ffffff\007"
