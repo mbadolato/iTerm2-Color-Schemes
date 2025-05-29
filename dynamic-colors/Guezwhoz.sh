11c86f4fa2b2b017d26dca7cabe970c7837c73cc
guesswhozzz <torridtime@gmail.com>
2025-05-29 18:47:53 +0300

    Update Guezwhoz scheme

--- dynamic-colors/Guezwhoz.sh
+++ dynamic-colors/Guezwhoz.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Guezwhoz
-printf "\033]4;0;#080808;1;#ff5f5f;2;#87d7af;3;#d7d787;4;#5fafd7;5;#afafff;6;#5fd7d7;7;#dadada;8;#8a8a8a;9;#d75f5f;10;#afd7af;11;#d7d7af;12;#87afd7;13;#afafd7;14;#87d7d7;15;#dadada\007"
-printf "\033]10;#d0d0d0;#1c1c1c;#eeeeee\007"
-printf "\033]17;#005f5f\007"
-printf "\033]19;#eeeeee\007"
+printf "\033]4;0;#333333;1;#f35782;2;#7fd693;3;#b4ca74;4;#60a0d7;5;#9a90e0;6;#69dad0;7;#dadada;8;#808080;9;#f35782;10;#b0d7af;11;#cbee7b;12;#52b5f4;13;#a698f5;14;#70e3d9;15;#ededed\007"
+printf "\033]10;#d9d9d9;#1e1e1e;#8cd7af\007"
+printf "\033]17;#195555\007"
+printf "\033]19;#8be0d8\007"
 printf "\033]5;0;#eeeeee\007"
--- pantheonterminal/Guezwhoz.sh
+++ pantheonterminal/Guezwhoz.sh
@@ -2,8 +2,8 @@
 dconf load /org/pantheon/terminal/settings/ <<COLORS
 [/]
 name='Guezwhoz'
-cursor-color='#eeeeee'
-foreground='#d0d0d0'
-background='rgba(28,28,28,.95)'
-palette='#080808:#ff5f5f:#87d7af:#d7d787:#5fafd7:#afafff:#5fd7d7:#dadada:#8a8a8a:#d75f5f:#afd7af:#d7d7af:#87afd7:#afafd7:#87d7d7:#dadada'
+cursor-color='#8cd7af'
+foreground='#d9d9d9'
+background='rgba(30,30,30,.95)'
+palette='#333333:#f35782:#7fd693:#b4ca74:#60a0d7:#9a90e0:#69dad0:#dadada:#808080:#f35782:#b0d7af:#cbee7b:#52b5f4:#a698f5:#70e3d9:#ededed'
 COLORS
