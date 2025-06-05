10f216f54a32cee6f1f2e3aa01812650a209c16b
Egor Lem <63231806+egorlem@users.noreply.github.com>
2025-06-05 23:23:09 +0300

    Update Guezwhoz theme (#565)
    * Update Guezwhoz scheme
    
    * Add and generate a theme from guezwhoz.yml
    
    * Update cyan color
    
    * Update bright white color

--- dynamic-colors/Guezwhoz.sh
+++ dynamic-colors/Guezwhoz.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Guezwhoz
-printf "\033]4;0;#080808;1;#ff5f5f;2;#87d7af;3;#d7d787;4;#5fafd7;5;#afafff;6;#5fd7d7;7;#dadada;8;#8a8a8a;9;#d75f5f;10;#afd7af;11;#d7d7af;12;#87afd7;13;#afafd7;14;#87d7d7;15;#dadada\007"
-printf "\033]10;#d0d0d0;#1c1c1c;#eeeeee\007"
-printf "\033]17;#005f5f\007"
-printf "\033]19;#eeeeee\007"
-printf "\033]5;0;#eeeeee\007"
+printf "\033]4;0;#333333;1;#e85181;2;#7ad694;3;#b7d074;4;#5aa0d6;5;#9a90e0;6;#58d6ce;7;#d9d9d9;8;#808080;9;#e85181;10;#afd7af;11;#d1ed85;12;#64b2ed;13;#a398ed;14;#61ede4;15;#ededed\007"
+printf "\033]10;#d9d9d9;#1d1d1d;#99d4b1\007"
+printf "\033]17;#245354\007"
+printf "\033]19;#58d6ce\007"
+printf "\033]5;0;#d9d9d9\007"
