d0d9dbe21c15701656faa2f820236dd1195c405a
Gregor Wünsch <41543402+gwuen@users.noreply.github.com>
2025-06-18 14:17:13 +0200

    Update Adwaita theme (#569)
    Update the Adwaita Dark theme to reflect the background color change
    performed in upstream libadwaita [^1]. Other colors are not affected.
    
    Also add YAML source files and regenerate other formats for both Adwaita
    and Adwaita Dark.
    
    [^1]: https://gitlab.gnome.org/GNOME/libadwaita/-/merge_requests/1317/diffs?commit_id=304338c4c7ebfc0601692db608afdf0202e96d8a

--- dynamic-colors/Adwaita Dark.sh
+++ dynamic-colors/Adwaita Dark.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Adwaita Dark
 printf "\033]4;0;#241f31;1;#c01c28;2;#2ec27e;3;#f5c211;4;#1e78e4;5;#9841bb;6;#0ab9dc;7;#c0bfbc;8;#5e5c64;9;#ed333b;10;#57e389;11;#f8e45c;12;#51a1ff;13;#c061cb;14;#4fd2fd;15;#f6f5f4\007"
-printf "\033]10;#ffffff;#1e1e1e;#ffffff\007"
+printf "\033]10;#ffffff;#1d1d20;#ffffff\007"
 printf "\033]17;#ffffff\007"
 printf "\033]19;#5e5c64\007"
 printf "\033]5;0;#ffffff\007"
