191d1104a8a0a7672427a53ddb4ffe5472c8264c
Hang Yin <h4x3rotab@gmail.com>
2025-08-08 13:43:49 -0700

    add Phala Green Dark theme

---
+++ dynamic-colors/Phala Green Dark.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Phala Green Dark
+printf "\033]4;0;#000000;1;#9d2814;2;#4aae31;3;#a9a733;4;#0c23b8;5;#b23bba;6;#4bb1bd;7;#cbcbcb;8;#797979;9;#da3b20;10;#5dd83f;11;#e9e74a;12;#1432f5;13;#da4be3;14;#64e5e8;15;#eaeaea\007"
+printf "\033]10;#cdfa50;#000000;#cdfa50\007"
+printf "\033]17;#1c4611\007"
+printf "\033]19;#ffffff\007"
+printf "\033]5;0;#defa86\007"
