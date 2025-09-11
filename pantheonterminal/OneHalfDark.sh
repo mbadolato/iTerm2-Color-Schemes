3cfe54bd2500ccf7dcf0665dc45d5fc9fa57efa5
j-c-m <5659031+j-c-m@users.noreply.github.com>
2025-09-11 22:55:45 +0000

    Auto-generate color schemes, screenshots, screenshots/README.md

--- pantheonterminal/OneHalfDark.sh
+++ pantheonterminal/OneHalfDark.sh
@@ -5,5 +5,5 @@ name='OneHalfDark'
 cursor-color='#a3b3cc'
 foreground='#dcdfe4'
 background='rgba(40,44,52,.95)'
-palette='#282c34:#e06c75:#98c379:#e5c07b:#61afef:#c678dd:#56b6c2:#dcdfe4:#282c34:#e06c75:#98c379:#e5c07b:#61afef:#c678dd:#56b6c2:#dcdfe4'
+palette='#282c34:#e06c75:#98c379:#e5c07b:#61afef:#c678dd:#56b6c2:#dcdfe4:#5d677a:#e06c75:#98c379:#e5c07b:#61afef:#c678dd:#56b6c2:#dcdfe4'
 COLORS
