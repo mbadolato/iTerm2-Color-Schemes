e9cf18f9d52b4a84426004429c8f61b6e6a956e7
Jesse Miller <jmiller@jmiller.com>
2025-09-14 10:20:02 -0600

    Add run-gen helper

---
+++ tools/run-gen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+source ../.venv/bin/activate
+pip install -r ../requirements.txt
+python gen.py
+mkdir -p ../terminal
+python iterm2terminal.py ../schemes ../terminal
+python -m screenshot_gen
+python generate_screenshots_readme.py
