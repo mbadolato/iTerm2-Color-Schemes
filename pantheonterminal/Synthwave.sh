9672cd11c276a453ae0c6c963e75a43dd66fa910
j-c-m <5659031+j-c-m@users.noreply.github.com>
2025-10-01 22:48:49 +0000

    Auto-generate color schemes, screenshots, screenshots/README.md

--- pantheonterminal/Synthwave.sh
+++ pantheonterminal/Synthwave.sh
@@ -5,5 +5,5 @@ name='Synthwave'
 cursor-color='#19cde6'
 foreground='#dad9c7'
 background='rgba(0,0,0,.95)'
-palette='#000000:#f6188f:#1ebb2b:#fdf834:#2186ec:#f85a21:#12c3e2:#ffffff:#000000:#f841a0:#25c141:#fdf454:#2f9ded:#f97137:#19cde6:#ffffff'
+palette='#000000:#f6188f:#1ebb2b:#fdf834:#2186ec:#f85a21:#12c3e2:#ffffff:#7f7094:#f841a0:#25c141:#fdf454:#2f9ded:#f97137:#19cde6:#ffffff'
 COLORS
