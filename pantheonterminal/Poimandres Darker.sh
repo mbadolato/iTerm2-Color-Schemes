4e6f91255181253d61960058bfe109246d51c934
bastiangx <bastiangx@pm.me>
2025-10-09 11:07:56 +0100

    pkg: gen Poimandres for all terminals

---
+++ pantheonterminal/Poimandres Darker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Poimandres Darker'
+cursor-color='#ffffff'
+foreground='#a6accd'
+background='rgba(22,22,30,.95)'
+palette='#16161e:#d0679d:#5de4c7:#fffac2:#89ddff:#fcc5e9:#add7ff:#ffffff:#a6accd:#d0679d:#5de4c7:#fffac2:#add7ff:#fae4fc:#89ddff:#ffffff'
+COLORS
