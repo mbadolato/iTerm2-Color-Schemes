2a70e3b0a1f4baa6b6dd72c5b018e40ab01aece5
Wiedy Mi <u.yakauleu@gmail.com>
2025-12-29 00:58:26 +0800

    Add Aizen Dark and Aizen Light themes (#650)

---
+++ pantheonterminal/Aizen Dark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Aizen Dark'
+cursor-color='#f8b080'
+foreground='#d0d6f0'
+background='rgba(26,26,26,.95)'
+palette='#1a1a1a:#f08898:#a4e09c:#f5dea4:#84b4f8:#c8a2f4:#90dcd0:#d0d6f0:#444444:#f08898:#a4e09c:#f5dea4:#84b4f8:#c8a2f4:#90dcd0:#ffffff'
+COLORS
