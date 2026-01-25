7b8cea21becbb05368d7a76bc71716f7a2ba1db0
Antônio Camargo <apcamargo@users.noreply.github.com>
2026-01-24 23:35:12 -0300

    Add the Kanso themes (#654)
    * Add the Kanso themes
    
    * Update CREDITS.md

---
+++ pantheonterminal/Kanso Pearl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Kanso Pearl'
+cursor-color='#22262d'
+foreground='#22262d'
+background='rgba(242,241,239,.95)'
+palette='#22262d:#c84053:#6f894e:#77713f:#4d699b:#b35b79:#597b75:#545464:#6d6f6e:#d7474b:#6e915f:#836f4a:#6693bf:#624c83:#5e857a:#43436c'
+COLORS
