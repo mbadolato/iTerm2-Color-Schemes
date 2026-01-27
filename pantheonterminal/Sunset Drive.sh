b15eca96ac706325d43d0fb1068b0f4ab3afefc0
Taha Nejad <paprikataha@gmail.com>
2026-01-27 03:38:15 -0800

    Add Sunset Drive theme

---
+++ pantheonterminal/Sunset Drive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Sunset Drive'
+cursor-color='#ededfe'
+foreground='#ededfe'
+background='rgba(15,15,26,.95)'
+palette='#0a0a13:#ff0063:#00f992:#ffe900:#00a4ff:#ff57fd:#00ffed:#ededff:#3e3e4b:#ff948b:#00fcb9:#ffff68:#3ea0ff:#ff93ff:#38ffff:#f8f8ff'
+COLORS
