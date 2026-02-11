901a0aee5d35ffd60b6f86487e25168f3ec9c597
Tiziano Müller <tm@dev-zero.ch>
2026-02-11 17:21:55 +0100

    Add Modus {Operandi,Vivendi} (including Tinted variants) (#658)
    obsoletes #655

---
+++ dynamic-colors/Modus Vivendi Tinted.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Modus Vivendi Tinted
+printf "\033]4;0;#0d0e1c;1;#ff5f59;2;#44bc44;3;#d0bc00;4;#2fafff;5;#feacd0;6;#00d3d0;7;#a6a6a6;8;#595959;9;#ff6b55;10;#00c06f;11;#fec43f;12;#79a8ff;13;#b6a0ff;14;#6ae4b9;15;#ffffff\007"
+printf "\033]10;#ffffff;#0d0e1c;#ff66ff\007"
+printf "\033]17;#555a66\007"
+printf "\033]19;#ffffff\007"
+printf "\033]5;0;#ffffff\007"
---
+++ pantheonterminal/Modus Vivendi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Modus Vivendi'
+cursor-color='#ffffff'
+foreground='#ffffff'
+background='rgba(0,0,0,.95)'
+palette='#000000:#ff5f59:#44bc44:#d0bc00:#2fafff:#feacd0:#00d3d0:#a6a6a6:#595959:#ff7f9f:#00c06f:#fec43f:#79a8ff:#b6a0ff:#6ae4b9:#ffffff'
+COLORS
