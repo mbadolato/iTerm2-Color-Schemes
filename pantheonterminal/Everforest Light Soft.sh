e95c31b06dcbb14fe7eb198efeef74b308175286
Erik Skultety <skultety.erik@gmail.com>
2026-05-15 08:14:19 +0200

    Add Everforest Light Soft color scheme
    Port the light soft (low) contrast variant of the Everforest color scheme
    from https://github.com/icewind/everforest.iterm2.
    
    Signed-off-by: Erik Skultety <skultety.erik@gmail.com>

---
+++ pantheonterminal/Everforest Light Soft.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Everforest Light Soft'
+cursor-color='#f57d26'
+foreground='#5c6a72'
+background='rgba(229,223,197,.95)'
+palette='#7a8478:#e67e80:#8da666:#c1a266:#72aea6:#c98ca9:#76b385:#a5a292:#99a393:#f85552:#8da101:#d29300:#3a94c5:#df69ba:#35a77c:#fffbef'
+COLORS
