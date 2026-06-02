fcbd8bc01cf7faf188eac4f410e5aa385bb334c4
Josh McKinney <joshka@users.noreply.github.com>
2026-06-02 02:07:02 -0700

    Add Aardvark Ink theme
    Add Aardvark Ink as a YAML source theme and generate the
    corresponding terminal theme files from that source.

---
+++ pantheonterminal/Aardvark Ink.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Aardvark Ink'
+cursor-color='#b4bcca'
+foreground='#b4bcca'
+background='rgba(15,20,31,.95)'
+palette='#222734:#c26265:#52aa60:#ad9b49:#487fd4:#af5bd1:#269d9a:#5a6377:#3a4152:#e48383:#75cf84:#c7b461:#76a8f2:#d58bf0:#52c4c0:#dfe5ee'
+COLORS
