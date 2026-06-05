b94016d9ae698545cfec472edf764ec355fda176
Mike McBride <mike@mcbrides.us>
2026-06-05 16:55:17 -0500

    Update Electron Highlighter + Day to refreshed palette
    Refresh the Electron Highlighter color family to the project's latest palette:
    - dark: retuned yellow + sky-cyan and a unified single-hue neutral ramp
      (comment / bright-black / bright-white, cursor text, selection)
    - Day: lighter Latte-style gold yellow
    
    Regenerated all terminal formats from the schemes/ + yaml/ sources via
    tools/gen.py (and iterm2terminal.py for the .terminal). Screenshots not
    regenerated.

--- dynamic-colors/Electron Highlighter.sh
+++ dynamic-colors/Electron Highlighter.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Electron Highlighter
-printf "\033]4;0;#15161f;1;#ff6c8d;2;#00ffc3;3;#ffd7a9;4;#77abff;5;#daa4f4;6;#00fdff;7;#778faf;8;#4a6789;9;#ff6c8d;10;#00ffc3;11;#ffd7a9;12;#77abff;13;#daa4f4;14;#00fdff;15;#c3cee2\007"
+printf "\033]4;0;#15161f;1;#ff6c8d;2;#00ffc3;3;#ffebb1;4;#77abff;5;#daa4f4;6;#00e5ff;7;#7187b7;8;#4b5b8c;9;#ff6c8d;10;#00ffc3;11;#ffebb1;12;#77abff;13;#daa4f4;14;#00e5ff;15;#c3cde2\007"
 printf "\033]10;#a5b6d4;#23283d;#a5b6d4\007"
-printf "\033]17;#25345a\007"
+printf "\033]17;#293257\007"
 printf "\033]19;#a5b6d4\007"
 printf "\033]5;0;#a8b5d1\007"
