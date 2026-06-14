67d949099978027c6831d2839a23d4b0cb94518b
Shafiq Jetha <shafiq.jetha@outlook.com>
2026-06-14 08:36:16 -0600

    New color schemes added: Sandstone Classic, Sandstone Ink, Sandstone Warm

---
+++ generic/Sandstone Classic.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+# Sandstone Classic
+
+# source for these helper functions:
+# https://github.com/chriskempson/base16-shell/blob/master/templates/default.mustache
+if [ -n "$TMUX" ]; then
+  # Tell tmux to pass the escape sequences through
+  # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+  put_template() { printf '\033Ptmux;\033\033]4;%d;rgb:%s\033\033\\\033\\' $@; }
+  put_template_var() { printf '\033Ptmux;\033\033]%d;rgb:%s\033\033\\\033\\' $@; }
+  put_template_custom() { printf '\033Ptmux;\033\033]%s%s\033\033\\\033\\' $@; }
+elif [ "${TERM%%[-.]*}" = "screen" ]; then
+  # GNU screen (screen, screen-256color, screen-256color-bce)
+  put_template() { printf '\033P\033]4;%d;rgb:%s\007\033\\' $@; }
+  put_template_var() { printf '\033P\033]%d;rgb:%s\007\033\\' $@; }
+  put_template_custom() { printf '\033P\033]%s%s\007\033\\' $@; }
+elif [ "${TERM%%-*}" = "linux" ]; then
+  put_template() { [ $1 -lt 16 ] && printf "\e]P%x%s" $1 $(echo $2 | sed 's/\///g'); }
+  put_template_var() { true; }
+  put_template_custom() { true; }
+else
+  put_template() { printf '\033]4;%d;rgb:%s\033\\' $@; }
+  put_template_var() { printf '\033]%d;rgb:%s\033\\' $@; }
+  put_template_custom() { printf '\033]%s%s\033\\' $@; }
+fi
+
+# 16 color space
+put_template 0  "07/36/42"
+put_template 1  "cf/25/22"
+put_template 2  "5d/7b/00"
+put_template 3  "86/5e/00"
+put_template 4  "0d/6e/b2"
+put_template 5  "c4/2a/78"
+put_template 6  "03/7a/71"
+put_template 7  "6e/68/55"
+put_template 8  "3d/52/59"
+put_template 9  "c0/40/10"
+put_template 10 "5e/71/71"
+put_template 11 "5b/70/70"
+put_template 12 "5c/6f/6f"
+put_template 13 "5f/64/b7"
+put_template 14 "5f/6e/6e"
+put_template 15 "fd/f6/e3"
+
+color_foreground="3d/52/59"
+color_background="fd/f6/e3"
+
+if [ -n "$ITERM_SESSION_ID" ]; then
+  # iTerm2 proprietary escape codes
+  put_template_custom Pg "3d5259"
+  put_template_custom Ph "fdf6e3"
+  put_template_custom Pi "3d5259"
+  put_template_custom Pj "e4dcc8"
+  put_template_custom Pk "a49d8a"
+  put_template_custom Pl "3d5259"
+  put_template_custom Pm "fdf6e3"
+else
+  put_template_var 10 $color_foreground
+  put_template_var 11 $color_background
+  if [ "${TERM%%-*}" = "rxvt" ]; then
+    put_template_var 708 $color_background # internal border (rxvt)
+  fi
+  put_template_custom 12 ";7" # cursor (reverse video)
+fi
+
+# clean up
+unset -f put_template
+unset -f put_template_var
+unset -f put_template_custom
+
+unset color_foreground
+unset color_background
---
+++ pantheonterminal/Sandstone Classic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Sandstone Classic'
+cursor-color='#3d5259'
+foreground='#3d5259'
+background='rgba(253,246,227,.95)'
+palette='#073642:#cf2522:#5d7b00:#865e00:#0d6eb2:#c42a78:#037a71:#6e6855:#3d5259:#c04010:#5e7171:#5b7070:#5c6f6f:#5f64b7:#5f6e6e:#fdf6e3'
+COLORS
