2e6668b6337ca316c5037192d34e22ccf2a77be4
Zasda Yusuf Mikail <zasdaym@gmail.com>
2026-08-18 04:14:47 +0700

    Add Grok Day and Grok Night so terminals can use the grok-themes palettes. (#736)
    Signed-off-by: Zasda Mikail <zasdaym@gmail.com>

---
+++ pantheonterminal/Grok Night.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Grok Night'
+cursor-color='#e0af68'
+foreground='#e1e1e1'
+background='rgba(20,20,20,.95)'
+palette='#0a0a0a:#f7768e:#9ece6a:#e0af68:#7aa2f7:#bb9af7:#7dcfff:#c8c8c8:#6c6c6c:#f7768e:#9ece6a:#e0af68:#7aa2f7:#bb9af7:#7dcfff:#e1e1e1'
+COLORS
