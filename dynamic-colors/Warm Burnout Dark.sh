80da45c9c77a66aea5ecadb405292cd245682b17
Felipe Lima <felipe@ferreiradelima.com>
2026-08-17 17:15:21 -0400

    Add Warm Burnout Dark and Warm Burnout Light themes (#735)
    Mostly warm palette with contrast-audited colors from the Warm Burnout suite.

---
+++ dynamic-colors/Warm Burnout Dark.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Warm Burnout Dark
+printf "\033]4;0;#23211b;1;#f06b73;2;#70bf56;3;#fdb04c;4;#4fbfff;5;#d0a1ff;6;#93e2c8;7;#c7c7c7;8;#686868;9;#f07178;10;#aad94c;11;#ffb454;12;#59c2ff;13;#d2a6ff;14;#95e6cb;15;#ffffff\007"
+printf "\033]10;#bfbdb6;#1a1510;#f5c56e\007"
+printf "\033]17;#33393a\007"
+printf "\033]19;#bfbdb6\007"
+printf "\033]5;0;#bfbdb6\007"
