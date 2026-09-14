ad96f8ab27fc5594c39398c7f1e5563abce4cdae
Jesse Brizzi <3254751+jes-bz@users.noreply.github.com>
2026-09-14 16:44:09 -0400

    Add Catthode theme (#741)
    Source and generated formats prepared with OpenAI Codex; palette provenance remains the Catthode project.

---
+++ generic/Catthode.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+# Catthode
+
+# source for these helper functions:
+# https://github.com/chriskempson/base16-shell/blob/master/templates/default.mustache
+if [ -n "$TMUX" ]; then
+  # Tell tmux to pass the escape sequences through
+  # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+  put_template() { printf '\033Ptmux;\033\033]4;%d;rgb:%s\033\033\\\033\\' $@; }
+  put_template_var() { printf '\033Ptmux;\033\033]%d;rgb:%s\033\033\\\033\\' $@; }
+  put_template_custom() { printf '\033Ptmux;\033\033]%s%s\033\033\\\033\\' $@; }
+elif [ "${TERM%%[-.]*}" = "screen" ]; then
+  # GNU screen (screen, screen-256color, screen-256color-bce)
+  put_template() { printf '\033P\033]4;%d;rgb:%s\007\033\\' $@; }
+  put_template_var() { printf '\033P\033]%d;rgb:%s\007\033\\' $@; }
+  put_template_custom() { printf '\033P\033]%s%s\007\033\\' $@; }
+elif [ "${TERM%%-*}" = "linux" ]; then
+  put_template() { [ $1 -lt 16 ] && printf "\e]P%x%s" $1 $(echo $2 | sed 's/\///g'); }
+  put_template_var() { true; }
+  put_template_custom() { true; }
+else
+  put_template() { printf '\033]4;%d;rgb:%s\033\\' $@; }
+  put_template_var() { printf '\033]%d;rgb:%s\033\\' $@; }
+  put_template_custom() { printf '\033]%s%s\033\\' $@; }
+fi
+
+# 16 color space
+put_template 0  "14/14/14"
+put_template 1  "ff/6b/6b"
+put_template 2  "b9/d6/65"
+put_template 3  "ff/b8/6c"
+put_template 4  "9c/d9/e6"
+put_template 5  "eb/a4/be"
+put_template 6  "ae/e6/d6"
+put_template 7  "b3/b3/b3"
+put_template 8  "63/63/63"
+put_template 9  "ff/6b/6b"
+put_template 10 "b9/d6/65"
+put_template 11 "fa/e2/c8"
+put_template 12 "9c/d9/e6"
+put_template 13 "eb/a4/be"
+put_template 14 "ae/e6/d6"
+put_template 15 "ff/ff/ff"
+
+color_foreground="ff/ff/ff"
+color_background="00/00/00"
+
+if [ -n "$ITERM_SESSION_ID" ]; then
+  # iTerm2 proprietary escape codes
+  put_template_custom Pg "ffffff"
+  put_template_custom Ph "000000"
+  put_template_custom Pi "fae2c8"
+  put_template_custom Pj "2d2d2d"
+  put_template_custom Pk "ffffff"
+  put_template_custom Pl "ff9e3b"
+  put_template_custom Pm "000000"
+else
+  put_template_var 10 $color_foreground
+  put_template_var 11 $color_background
+  if [ "${TERM%%-*}" = "rxvt" ]; then
+    put_template_var 708 $color_background # internal border (rxvt)
+  fi
+  put_template_custom 12 ";7" # cursor (reverse video)
+fi
+
+# clean up
+unset -f put_template
+unset -f put_template_var
+unset -f put_template_custom
+
+unset color_foreground
+unset color_background
---
+++ pantheonterminal/Catthode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Catthode'
+cursor-color='#ff9e3b'
+foreground='#ffffff'
+background='rgba(0,0,0,.95)'
+palette='#141414:#ff6b6b:#b9d665:#ffb86c:#9cd9e6:#eba4be:#aee6d6:#b3b3b3:#636363:#ff6b6b:#b9d665:#fae2c8:#9cd9e6:#eba4be:#aee6d6:#ffffff'
+COLORS
